fb1658a629ac7ca0e06bd0227cbaf2f47afeb12c
programadormovel <programadormovel@gmail.com>
2023-05-18 11:03:34 -0300

    entendi nada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         // Aula introdutória de Orientação à Objetos
+        // Declaração de objeto
+        Pessoa adao;
+        // Instanciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
 
 
     }
---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+    // Atributos da classe
+    public String nome;
+    public String sobrenome;
+    // métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public String falar(String texto){
+        return texto;
+    }
+
+    public Pessoa criarPessoa(){
+        return new Pessoa();
+    }
+}
